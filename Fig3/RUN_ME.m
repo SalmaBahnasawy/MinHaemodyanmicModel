94401a2e5b165060e38187e4c6f8cd628b94f20c
SalmaBahnasawy <salma.bahnasawy@pharma.asu.edu.eg>
2020-04-05 20:26:02 +1200

    Create RUN_ME.m

---
+++ Fig3/RUN_ME.m
@@ -0,0 +1,11 @@
+%% Minimal model descriping short-term haemidynamic changes of CVS system
+% Code to get figure 3
+
+
+clear all % clears variable cache
+clc  % clears screen
+
+%% Set initial conditions & run simulation
+run_ode_fig3
+% Output visualisation
+paper_overlay_fig3
