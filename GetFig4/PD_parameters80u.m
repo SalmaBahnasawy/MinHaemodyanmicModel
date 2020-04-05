afc911a062576f25bd95fef349d428c002878bbc
SalmaBahnasawy <salma.bahnasawy@pharma.asu.edu.eg>
2020-04-05 21:31:49 +1200

    Create PD_parameters80u.m

---
+++ GetFig4/PD_parameters80u.m
@@ -0,0 +1,27 @@
+%% Drug parameters
+
+%PD
+Imax=0.38;
+C50=55; %ng/ml reported c50=105 nmol/l, Mwt=267.364 gm/mol
+GAMA=1;
+uCp=[0
+3.48
+68.97
+113.36
+122.93
+117.7
+115.32
+109.44
+100.55
+89.65
+80.3
+
+]'; %ng/ml
+
+for x=1:length(uCp)
+    I(:,x)=(Imax*uCp(:,x)^GAMA)./(C50+uCp(:,x)^GAMA); 
+end
+
+%% system parameters
+
+KOUT=35/60;%min-1
